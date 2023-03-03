05359588d33c71c95983814b776cd93972f0d756
yosibagus <yosibagusdsd@gmail.com>
2023-03-03 07:28:58 +0700

    update footer

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -972,7 +972,7 @@
                 <footer class="footer position-absolute">
                     <div class="row g-0 justify-content-between align-items-center h-100">
                         <div class="col-12 col-sm-auto text-center">
-                            <p class="mb-0 mt-2 mt-sm-0 text-900">Ruang Algoritma<span class="d-none d-sm-inline-block"></span><span class="mx-1">|</span><br class="d-sm-none" />2022 &copy;<a class="ms-1" href="https://yosibgsdr.site/">Yosi Bagus Sadar Rasuli</a></p>
+                            <p class="mb-0 mt-2 mt-sm-0 text-900">Ruang Algoritma<span class="d-none d-sm-inline-block"></span><span class="mx-1">|</span><br class="d-sm-none" />2022 &copy;<a class="ms-1" href="https://yosibgsdr.site/">Team IT</a></p>
                         </div>
                         <div class="col-12 col-sm-auto text-center">
                             <p class="mb-0 text-600">v1.0.0</p>
